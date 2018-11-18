abe3ea864096c8a1ed4aeef8276666e5088f3d48
Andrea Brucculeri <andrea@brucculeri.com>
2018-11-17 21:30:33 -0500

    third

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,13 @@
 $(function(){
-  console.log('scripts loaded');
-  var myKey = config.MY_KEY;
+  console.log('scripts loaded!');
+  //var myKey = config.MY_KEY;
   var url= 'http://api.open-notify.org/iss-now.json' ;
-  //you put your API key at the end of the thing which means you have to delete the one that is there
-  //delete the topic and put your own uses a plus as spaces
   var url2= 'https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=-34.44076&lon=-58.70521';
   var data= [];
   var html= '';
   var urlArray=[url, url2];
   var i='';
-console.log(data.iss_position.latitude);
+console.log('hi');
   // for(i=0; i<urlArray.length; i++){
 
     $.ajax({
@@ -20,7 +18,7 @@ console.log(data.iss_position.latitude);
       data: data,
       error: function(url, html){ console.log('Your ajax call failed. Sorry fam.'); console.log(data)},
       success:function(data){
-        console.log(data.iss_position.latitude);
+        console.log('it worked!');
         var lat=data.iss_position.latitude;
         var lon=data.iss_position.longitude;
         html += 'The space station is at ' + data.iss_position.latitude;
